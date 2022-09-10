90cc3f9386382cf447435903efb235448233fe15
psj <271533323@qq.com>
2022-09-10 17:57:38 +0800

    fix: 改用helper-plugin-utils

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { type PluginObj, transform, NodePath } from "@babel/core";
-import { thisExpression, identifier } from "@babel/types";
+import { transform } from "@babel/core";
+import arrowFunctionPlugin from "./src/template/arrowFunction-plugin";
 
 const code = `
 var _this = "123";
@@ -7,50 +7,6 @@ const arrowFunc = () => {
 	console.log(this)
 }
 `;
-function generateBindName(path: NodePath, name = "_this"): string {
-  if (!path.scope.hasBinding(name)) {
-    return name;
-  }
-  let number = 0;
-  while (true) {
-    let suffixname = name + ++number;
-    if (!path.scope.hasBinding(suffixname)) {
-      return suffixname;
-    }
-  }
-}
-
-function hoistFunctionEnvironment(nodePath: NodePath) {
-  const thisEnv = nodePath.findParent((p) => {
-    return (p.isFunction() && !p.isArrowFunctionExpression) || p.isProgram();
-  })!;
-
-  const newThisName = generateBindName(thisEnv);
-
-  thisEnv.scope.push({
-    id: identifier(newThisName),
-    init: thisExpression(),
-  });
-
-  nodePath.traverse({
-    ThisExpression(thisPath) {
-      const replaceNode = identifier(newThisName);
-      thisPath.replaceWith(replaceNode);
-    },
-  });
-}
-
-function arrowFunctionExpression(nodePath: NodePath) {
-  const { node } = nodePath;
-  hoistFunctionEnvironment(nodePath);
-  node.type = "FunctionDeclaration";
-}
-
-const arrowFunctionPlugin: PluginObj = {
-  visitor: {
-    ArrowFunctionExpression: arrowFunctionExpression,
-  },
-};
 
 const resCode = transform(code, {
   plugins: [arrowFunctionPlugin],
--- src/template/arrowFunction-plugin.ts
+++ src/template/arrowFunction-plugin.ts
@@ -1,63 +1,68 @@
-import type { PluginObj, NodePath } from "@babel/core";
-import { thisExpression, identifier } from "@babel/types";
+import type { NodePath } from "@babel/core";
 
-/**
- * 根据作用域生成一个作用域变量名称
- */
-function generateBindName(path: NodePath, name = "_this"): string {
-  if (!path.scope.hasBinding(name)) {
-    return name;
+import { declare } from "@babel/helper-plugin-utils";
+
+const arrowFunctionPlugin = declare((api, options, dirname) => {
+  /**
+   * 处理匿名函数
+   */
+  function arrowFunctionExpression(nodePath: NodePath) {
+    const { node } = nodePath;
+    // 处理作用域环境
+    hoistFunctionEnvironment(nodePath);
+    // 将函数类型改为FunctionDeclaration
+    node.type = "FunctionDeclaration";
   }
-  let number = 0;
-  while (true) {
-    // 如果作用域含有该函数 则根据累加的number重新生成，直到生成到没有的作用域为止
-    let suffixname = name + ++number;
-    if (!path.scope.hasBinding(suffixname)) {
-      return suffixname;
+
+  /**
+   * 根据作用域生成一个作用域变量名称
+   */
+  function generateBindName(path: NodePath, name = "_this"): string {
+    if (!path.scope.hasBinding(name)) {
+      return name;
+    }
+    let number = 0;
+    while (true) {
+      // 如果作用域含有该函数 则根据累加的number重新生成，直到生成到没有的作用域为止
+      let suffixname = name + ++number;
+      if (!path.scope.hasBinding(suffixname)) {
+        return suffixname;
+      }
     }
   }
-}
-/**
- * 处理箭头函数作用域环境
- */
-function hoistFunctionEnvironment(nodePath: NodePath) {
-  // 向上寻找的作用域函数，直到顶级为止。
-  const thisEnv = nodePath.findParent((p) => {
-    return (p.isFunction() && !p.isArrowFunctionExpression) || p.isProgram();
-  })!;
 
-  // 新的this名称
-  const newThisName = generateBindName(thisEnv);
+  /**
+   * 处理箭头函数作用域环境
+   */
+  function hoistFunctionEnvironment(nodePath: NodePath) {
+    // 向上寻找的作用域函数，直到顶级为止。
+    const thisEnv = nodePath.findParent((p) => {
+      return (p.isFunction() && !p.isArrowFunctionExpression) || p.isProgram();
+    })!;
 
-  // 将当前的声明加入作用域
-  thisEnv.scope.push({
-    id: identifier(newThisName),
-    init: thisExpression(),
-  });
+    // 新的this名称
+    const newThisName = generateBindName(thisEnv);
 
-  // 替换原有this
-  nodePath.traverse({
-    ThisExpression(thisPath) {
-      const replaceNode = identifier(newThisName);
-      thisPath.replaceWith(replaceNode);
+    // 将当前的声明加入作用域
+    thisEnv.scope.push({
+      id: api.types.identifier(newThisName),
+      init: api.types.thisExpression(),
+    });
+
+    // 替换原有this
+    nodePath.traverse({
+      ThisExpression(thisPath) {
+        const replaceNode = api.types.identifier(newThisName);
+        thisPath.replaceWith(replaceNode);
+      },
+    });
+  }
+
+  return {
+    visitor: {
+      ArrowFunctionExpression: arrowFunctionExpression,
     },
-  });
-}
-/**
- * 处理匿名函数
- */
-function arrowFunctionExpression(nodePath: NodePath) {
-  const { node } = nodePath;
-  // 处理作用域环境
-  hoistFunctionEnvironment(nodePath);
-  // 将函数类型改为FunctionDeclaration
-  node.type = "FunctionDeclaration";
-}
-
-const arrowFunctionPlugin: PluginObj = {
-  visitor: {
-    ArrowFunctionExpression: arrowFunctionExpression,
-  },
-};
+  };
+});
 
 export default arrowFunctionPlugin;
